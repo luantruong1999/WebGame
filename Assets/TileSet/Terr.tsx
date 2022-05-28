a7a50884d9b75eb98144bc5bef79ce384c63a1a7
Luan <luantruong1999@gmail.com>
2022-05-28 15:37:14 +0700

    First

---
+++ Assets/TileSet/Terr.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Terr" tilewidth="100" tileheight="100" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="100" source="1 (1).png"/>
+ </tile>
+ <tile id="1">
+  <image width="100" height="100" source="2 (1).png"/>
+ </tile>
+ <tile id="2">
+  <image width="100" height="100" source="3 (1).png"/>
+ </tile>
+ <tile id="3">
+  <image width="100" height="100" source="4 (1).png"/>
+ </tile>
+ <tile id="4">
+  <image width="100" height="100" source="5 (1).png"/>
+ </tile>
+ <tile id="5">
+  <image width="100" height="100" source="6 (1).png"/>
+ </tile>
+ <tile id="6">
+  <image width="100" height="100" source="7 (1).png"/>
+ </tile>
+ <tile id="7">
+  <image width="100" height="100" source="8 (1).png"/>
+ </tile>
+ <tile id="8">
+  <image width="100" height="100" source="9 (1).png"/>
+ </tile>
+ <tile id="9">
+  <image width="100" height="100" source="10 (1).png"/>
+ </tile>
+ <tile id="10">
+  <image width="100" height="100" source="11 (1).png"/>
+ </tile>
+</tileset>
